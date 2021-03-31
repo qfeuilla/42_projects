a1afd6d29d2d50a746e5b72a7c9c6aaee28e49a2
Quentin FEUILLADE--MONTIXI <quentin.feuillade33@gmail.com>
2021-03-31 02:32:32 +0200

    puzzle more advance

--- n_puzzle/puzzle.py
+++ n_puzzle/puzzle.py
@@ -13,10 +13,12 @@ class Puzzle():
 		self.solution_maps = []
 		print("map to solve : ")
 		self.show_map(self.current_map)
+		self.answer = self.generate_answer()
 
 	def map_from_file(self, file):
 		data = open(file)
 		readed = []
+		all_nb = []
 		for i, line in enumerate(data.readlines()):
 			line = line.replace("#", " # ")
 			line = line.split()
@@ -40,14 +42,18 @@ class Puzzle():
 			if (len(int_data) != self.size):
 				raise Exception("each lines should have {} values".format(self.size))
 			readed.append(int_data)
+			all_nb += int_data
 		if len(readed) != self.size:
 			raise Exception("The map must be of size {}x{}".format(self.size, self.size))
-		
-		return readed
+		max_val = self.size ** 2
+		for i in range(max_val):
+			if all_nb.count(i) != 1:
+				raise Exception("The map should contain one value of each number in the range [0, {}[".format(max_val))
+		return np.array(readed, dtype=np.int)
 
 	def random_map(self):
 		all_num = list(range(self.size ** 2))
-		generated = np.zeros((self.size, self.size))
+		generated = np.zeros((self.size, self.size), dtype=np.int)
 		for y in range(self.size):
 			for x in range(self.size):
 				choice = np.random.choice(all_num)
@@ -59,5 +65,73 @@ class Puzzle():
 		if data is None:
 			data = self.current_map
 		print('\n'.join([' '.join([str(int(l)).center(5, ' ') for l in lst]) for lst in data]))
+	
+	def inversion_num(self):
+		ordered_map = list()
+		x = 0
+		y = 0
 
-Puzzle(use_file=True, input_file="test_map")
+		for i in range(self.size * 2 - 1):
+			res = i % 4
+			if i > 0:
+				x += (1 if res == 0 else -1) if not res % 2 else 0
+				y += (1 if res == 1 else -1) if res % 2 else 0
+			while ((x < self.size and res == 0) 
+					or (y < self.size and res == 1) 
+					or (x >= 0 and res == 2) 
+					or (y >= 0 and res == 3)) and self.current_map[y][x] not in ordered_map:
+				ordered_map.append(self.current_map[y][x])
+				x += (1 if res == 0 else -1) if not res % 2 else 0
+				y += (1 if res == 1 else -1) if res % 2 else 0
+			x += (-1 if res == 0 else 1) if not res % 2 else 0
+			y += (-1 if res == 1 else 1) if res % 2 else 0
+		reverse_global = 0
+		map_index = {}
+		for i, k in enumerate(ordered_map):
+			map_index[k] = i
+		for k in map_index.keys():
+			curr_ind = map_index[k]
+			for smaller in range(1, k):
+				if map_index[smaller] > curr_ind:
+					reverse_global += 1
+		return reverse_global
+
+	def is_sovable(self):
+		div = not self.inversion_num() % 2
+		if self.size % 2 == 1:
+			return div
+		else:
+			y, x = np.where(self.current_map == 0)
+			if y % 2 == 0:
+				return not div
+			else:
+				return div
+
+	def generate_answer(self):
+		answ = np.zeros((self.size, self.size), dtype=np.int)
+		current = 1
+		x = 0
+		y = 0
+		for i in range(self.size * 2 - 1):
+			res = i % 4
+			if i > 0:
+				x += (1 if res == 0 else -1) if not res % 2 else 0
+				y += (1 if res == 1 else -1) if res % 2 else 0
+			while ((x < self.size and res == 0) 
+					or (y < self.size and res == 1) 
+					or (x >= 0 and res == 2) 
+					or (y >= 0 and res == 3)) and answ[y][x] == 0:
+				answ[y][x] = current
+				current += 1
+				x += (1 if res == 0 else -1) if not res % 2 else 0
+				y += (1 if res == 1 else -1) if res % 2 else 0
+			x += (-1 if res == 0 else 1) if not res % 2 else 0
+			y += (-1 if res == 1 else 1) if res % 2 else 0
+		answ[y][x] = 0
+		return answ
+
+puzzle = Puzzle(use_file=True, input_file="test_map")
+
+print(puzzle.inversion_num())
+print(puzzle.is_sovable())
+print(puzzle.generate_answer())
